ef2711feed20c44529fcad34c20b130a55afa8f7
Ivan Norderhaug <norderhaug@protonmail.com>
2024-03-05 15:45:54 +0100

    added Multicast class

---
+++ multicast.py
@@ -0,0 +1,23 @@
+import socket
+
+"""
+    Multicast class to send and receive messages
+"""
+class Multicast:
+    def __init__(self, group, port):
+        self.group = group
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32) # time to live
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0) # 0 = no loopback, 1 = loopback
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', port))
+        mreq = socket.inet_aton(group) + socket.inet_aton("0.0.0.0") 
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq) # join the multicast group
+
+    def send_message(self, message):
+        self.sock.sendto(message.encode(), (self.group, self.port))
+
+    def receive_message(self):
+        data, address = self.sock.recvfrom(1024) # buffer size is 1024 bytes
+        return data.decode(), address
