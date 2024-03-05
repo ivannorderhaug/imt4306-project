0974f6fe15e5302b006687c5cff5426222e980aa
Ivan Norderhaug <norderhaug@protonmail.com>
2024-03-05 16:11:08 +0100

    update multicast.py

--- multicast.py
+++ multicast.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 """
     Multicast class to send and receive messages
@@ -12,11 +13,13 @@ class Multicast:
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0) # 0 = no loopback, 1 = loopback
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('', port))
-        mreq = socket.inet_aton(group) + socket.inet_aton("0.0.0.0") 
+        mreq = struct.pack('4sl', socket.inet_aton(group), socket.INADDR_ANY)
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq) # join the multicast group
 
     def send_message(self, message):
-        self.sock.sendto(message.encode(), (self.group, self.port))
+        if isinstance(message, str):
+            message = message.encode()
+        self.sock.sendto(message, (self.group, self.port))
 
     def receive_message(self):
         data, address = self.sock.recvfrom(1024) # buffer size is 1024 bytes
